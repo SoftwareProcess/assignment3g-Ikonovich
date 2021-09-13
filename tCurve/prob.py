93cfebca1e8e0873012d44c2e716440e24b34ade
Evan <evanhnly@gmail.com>
2021-09-12 20:31:39 -0500

    Green Light |

--- tCurve/prob.py
+++ tCurve/prob.py
@@ -107,5 +107,46 @@ def _f(u, n):
 
 # ----------- PLEASE COMPLETE THE FUNCTION BELOW ----------
 def _integrate(t, n, _f):
+
+    maxError = 0.001
+    simpsonOld = 0.0
+    simpsonNew = maxError
     
-    return integrateTest(t, n, _f)
+    lowBound = 0
+    highBound = 16
+    divisions = 4
+    
+    while (abs((simpsonNew - simpsonOld) / simpsonNew) > maxError):
+        simpsonOld = simpsonNew
+        width = (highBound - lowBound) / divisions
+        
+        total = 0
+        i = 1
+        currentWidth = 0
+        
+        for x in range(divisions):
+
+           
+            if (x == 0):
+                total += _f(currentWidth, n)
+                i += 1
+                
+            elif (x == divisions):
+                total += _f(currentWidth, n)
+                
+            elif (i == 2):
+                total += 2 * _f(currentWidth, n)
+                i += 1
+                
+            elif(i == 3):
+                total += 4 * _f(currentWidth, n)
+                i = 2
+                
+                
+            currentWidth += width
+            
+        simpsonNew = (width/3) * total
+        
+        divisions = divisions * 2
+        
+    return simpsonNew
